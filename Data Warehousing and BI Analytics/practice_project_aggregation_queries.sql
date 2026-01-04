da889c53f73d633dd24f726f87577cd108554377
Jacob Christensen <christensenjacob763@gmail.com>
2026-01-04 12:49:10 -0800

    Create practice_project_aggregation_queries.sql

---
+++ Data Warehousing and BI Analytics/practice_project_aggregation_queries.sql
@@ -0,0 +1,83 @@
+-- The Practice Project exercises
+-- Comes after creating the table schema
+-- and populating said tables with data.
+-- Writing aggregation queries and creating
+-- materialized views.
+
+-- Create a grouping sets query
+-- Create a grouping sets query using the columns productid, producttype, total sales. 
+SELECT
+    p.Productid,
+    p.Producttype,
+    SUM(f.Price_PerUnit * f.QuantitySold) AS TotalSales
+FROM
+    FactSales f
+INNER JOIN
+    DimProduct p ON f.Productid = p.Productid
+GROUP BY GROUPING SETS (
+    (p.Productid, p.Producttype),
+    p.Productid,
+    p.Producttype,
+    ()
+)
+ORDER BY
+    p.Productid,
+    p.Producttype;
+
+-- Create a rollup query
+-- Create a rollup query using the columns year, city, productid, and total sales.
+SELECT
+    d.Year,
+    cs.City,
+    p.Productid,
+    SUM(f.Price_PerUnit * f.QuantitySold) AS TotalSales 
+FROM
+    FactSales f
+JOIN
+    DimDate d ON f.Dateid = d.Dateid
+JOIN
+    DimProduct p ON f.Productid = p.Productid
+JOIN
+    DimCustomerSegment cs ON f.Segmentid = cs.Segmentid
+GROUP BY ROLLUP (d.Year, cs.City, p.Productid)
+ORDER BY
+    d.Year DESC,
+    cs.City,
+    p.Productid;
+
+-- Create a cube query
+-- Create a cube query using the columns year, city, productid, and average sales.
+SELECT
+    d.Year,
+    cs.City,
+    p.Productid,
+    AVG(f.Price_PerUnit * f.QuantitySold) AS AverageSales
+FROM
+    FactSales f
+INNER JOIN
+    DimDate d ON f.Dateid = d.Dateid
+INNER JOIN
+    DimProduct p ON f.Productid = p.Productid
+INNER JOIN
+    DimCustomerSegment cs ON f.Segmentid = cs.Segmentid
+GROUP BY CUBE (d.Year, cs.City, p.Productid);
+
+-- Create a materialized view
+-- Create an materialized view named max_sales using the columns city, productid, producttype, and max sales.
+CREATE MATERIALIZED VIEW max_sales AS
+SELECT
+    cs.City,
+    p.Productid,
+    p.Producttype,
+    MAX(f.Price_PerUnit * f.QuantitySold) AS MaxSales
+FROM
+    FactSales f
+JOIN
+    DimProduct p ON f.Productid = p.Productid
+JOIN
+    DimCustomerSegment cs ON f.Segmentid = cs.Segmentid
+GROUP BY
+    cs.City,
+    p.Productid,
+    p.Producttype
+WITH DATA;
